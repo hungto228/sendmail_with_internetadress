038af9aa390bf8496fddca4ab99d7892a40f5d1e
hungto228 <hungto228@gmail.com>
2020-12-03 14:39:56 +0700

    update application.yml

--- src/main/java/com/example/sendmail_reciver/service/SendMailService.java
+++ src/main/java/com/example/sendmail_reciver/service/SendMailService.java
@@ -11,9 +11,14 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.List;
 import java.util.Properties;
 @Service
@@ -21,11 +26,27 @@ public class SendMailService {
     @Autowired
     private UsersRepository usersRepository;
     @Autowired SendMailService sendMailService;
-    private static final String SMTP_HOST_NAME = "smtp.googlemail.com";
-    private static final String SMTP_AUTH_USER = "hungto2288@gmail.com";
-    private static final String SMTP_AUTH_PASSWORD = "baybay99";
-    private static final String EMAIL_SUBJECT="SSS333";
-    private static final String EMAIL_CONTENT="SSS";
+//    private static final String SMTP_HOST_NAME = "smtp.googlemail.com";
+//    private static final String SMTP_AUTH_USER = "hungto2288@gmail.com";
+//    private static final String SMTP_AUTH_PASSWORD = "baybay99";
+//    private static final String EMAIL_SUBJECT="SSS333";
+//    private static final String EMAIL_CONTENT="SSS";
+//    private static final String PATH_IMAGE="C:/Users/Admin/Desktop/imageapp/dangnhap.png";
+    @Value("${spring.mail.host}")
+    String SMTP_HOST_NAME ;
+    @Value("${spring.mail.username}")
+    private  String SMTP_AUTH_USER ;
+    @Value("${spring.mail.password}")
+    private  String SMTP_AUTH_PASSWORD;
+    @Value("${spring.mail.port}")
+    private String PORT;
+    @Value("${EMAIL_SUBJECT}")
+    private String EMAIL_SUBJECT;
+    @Value("${EMAIL_CONTENT}")
+    private String EMAIL_CONTENT;
+    @Value("${PATH_IMAGE}")
+    private String PATH_IMAGE;
+
     @Scheduled
     public void sendEmail(String[] recipientsList, String emailSubject,String emailContent, String senderAddress)
             throws MessagingException {
@@ -50,7 +71,34 @@ public class SendMailService {
         }
         emailMessage.setRecipients(Message.RecipientType.TO, recipientInternetAddress);
         emailMessage.setSubject(emailSubject);
-        emailMessage.setContent(emailContent, "text/plain");
+        // This mail has 2 part, the BODY and the embedded image
+        MimeMultipart multipart = new MimeMultipart("related");
+        BodyPart messageBodyPart = new MimeBodyPart();
+
+        //create htmltext convert to image
+        // add img src to show image
+        String htmlText = EMAIL_CONTENT +"<br/><img src=\"cid:image\">";
+
+        messageBodyPart.setContent(htmlText, "text/html");
+        // add it
+        multipart.addBodyPart(messageBodyPart);
+
+        // second part (the image)
+        messageBodyPart = new MimeBodyPart();
+        DataSource fds = new FileDataSource(
+                PATH_IMAGE);
+
+        messageBodyPart.setDataHandler(new DataHandler(fds));
+        messageBodyPart.setFileName(EMAIL_SUBJECT);
+        messageBodyPart.setHeader("Content-ID", "<image>");
+
+        // add image to the multipart
+        multipart.addBodyPart(messageBodyPart);
+
+        emailMessage.setContent(multipart, "text/html; charset=utf-8");
+        // emailMessage.setContent(multipart, "text/plain");
+        // emailMessage.setContent(emailContent, "text/html; charset=utf-8");
+
         Transport.send(emailMessage);
     }
     private class CredentialsAuthenticator extends javax.mail.Authenticator {
